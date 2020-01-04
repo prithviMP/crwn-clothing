6b1465d5ba4a2e8ed27f4e1693b8f07ee81d0728
prithvimp <prithvihhh@gmial.com>
2020-01-04 12:04:18 -0800

    update

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+import { Switch, Route } from "react-router-dom";
+
+import HomePage from "./pages/homepage/homepage.componet";
+import ShopPage from "./pages/shop/shop.component";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
