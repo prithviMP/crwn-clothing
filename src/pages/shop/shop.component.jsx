14796911f45f482783a03980e8833cce3633798b
prithvimp <prithvihhh@gmail.com>
2020-05-15 10:21:54 +0530

    updating the whole code

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import SHOP_DATA from "./shop.data.js";
+import SHOP_DATA from './shop.data.js';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionPreview from '../../components/collection-preview/collection-preview';
 
 class ShopPage extends React.Component {
   constructor(props) {
@@ -16,9 +16,9 @@ class ShopPage extends React.Component {
   render() {
     const { collections } = this.state;
     return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollections }) => (
-          <CollectionPreview key={id} {...otherCollections} />
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
       </div>
     );
